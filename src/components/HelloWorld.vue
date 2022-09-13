9b345a251e69a40f94c522ad6b5fb305317e37fd
agilusinformatica <claudio@agilus.com.br>
2022-09-13 16:47:15 -0300

    implantando exemplo de Grid Devextreme

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,97 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <DxDataGrid 
+      id="dataGrid"
+      :data-source="empresas" 
+      key-expr="Id" 
+      :allow-column-reordering="true" 
+      :column-auto-width="false" 
+      @exporting="exportGrid" 
+      :allow-column-resizing="true"
+    >
+      <DxColumnChooser :enabled="true" />
+      <DxStateStoring
+        :enabled="true"
+        type="localStorage"
+        storage-key="AgilusGrid"
+      />
+      <DxFilterRow :visible="true" />
+      <DxSearchPanel :visible="true" />
+      <DxGroupPanel :visible="true" />
+      <DxSelection mode="single" />
+      <DxExport :enabled="true" :formats="['xlsx', 'pdf']" />
+      <DxSummary>
+          <DxGroupItem
+              summary-type="count"
+          />
+          <DxGroupItem
+                column="LimiteUsuarios"
+                summary-type="sum"
+                :align-by-column="true"
+                :show-in-group-footer="false"
+            />
+          <DxTotalItem
+                column="LimiteUsuarios"
+                summary-type="sum"
+            />
+      </DxSummary>
+    </DxDataGrid>
   </div>
 </template>
 
 <script>
+import 'devextreme/dist/css/dx.light.css';
+import axios from 'axios'
+import { DxDataGrid, DxFilterRow, DxSearchPanel, DxGroupPanel, DxSelection, DxSummary, DxGroupItem, DxTotalItem, DxExport, DxStateStoring, DxColumnChooser } from 'devextreme-vue/data-grid';
+import { Workbook } from 'exceljs';
+import saveAs from 'file-saver';
+import { exportDataGrid } from 'devextreme/excel_exporter';
+import { jsPDF } from 'jspdf';
+import { exportDataGrid as exportDataGridToPdf} from 'devextreme/pdf_exporter';
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+    components: {
+      DxDataGrid, DxFilterRow, DxSearchPanel, DxGroupPanel, DxSelection, DxSummary, DxGroupItem, DxTotalItem, DxExport, DxStateStoring, DxColumnChooser
+    },
+    data() {
+        return {
+          empresas: []
+        }
+    },
+    mounted () {
+      axios('http://localhost:55461/api/empresas/listar')
+        .then(response => {
+          this.empresas = response.data
+        })
+    },
+    methods: {
+        exportGrid(e) {
+            if (e.format === 'xlsx') {
+                const workbook = new Workbook(); 
+                const worksheet = workbook.addWorksheet("Main sheet"); 
+                exportDataGrid({ 
+                    worksheet: worksheet, 
+                    component: e.component,
+                }).then(function() {
+                    workbook.xlsx.writeBuffer().then(function(buffer) { 
+                        saveAs(new Blob([buffer], { type: "application/octet-stream" }), "DataGrid.xlsx"); 
+                    }); 
+                }); 
+                e.cancel = true;
+            } 
+            else if (e.format === 'pdf') {
+                const doc = new jsPDF();
+                exportDataGridToPdf({
+                    jsPDFDocument: doc,
+                    component: e.component,
+                }).then(() => {
+                    doc.save('DataGrid.pdf');
+                });
+            }
+        }
+    }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
